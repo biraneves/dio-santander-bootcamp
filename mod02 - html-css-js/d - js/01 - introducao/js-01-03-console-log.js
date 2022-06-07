41818848cdeda193f328fc8e39ca637e48453d5d
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-07 08:23:38 -0300

    feat(site): impressão no console

---
+++ mod02 - html-css-js/d - js/01 - introducao/js-01-03-console-log.js
@@ -0,0 +1,21 @@
+function returnEvenNums(array) {
+
+    let evenNums = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i] % 2 === 0) {
+
+            evenNums.push(array[i]);
+
+        }
+
+    }
+
+    console.log(evenNums);
+
+}
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+returnEvenNums(array);
