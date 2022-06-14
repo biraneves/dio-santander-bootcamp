5757a2231d98ab95ea9dd92907bf4e403e44f438
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-14 11:28:08 -0300

    feat: exemplos de const, let e var

---
+++ mod02 - html-css-js/d - js/03 - variaveis e tipos/aula-01/aula-01-03.js
@@ -0,0 +1,11 @@
+var firstName = "João";
+var lastName = "Souza";
+
+if (firstName === "João") {
+
+    var firstName = "Pedro";
+    let lastName = "Silva";
+
+}
+
+console.log(firstName, lastName);
